9f8ba32de784fde792d678e544be3e55ef4a4877
David Capello <davidcapello@gmail.com>
2018-08-30 16:29:08 -0300

    We need <sys/sysmacros.h> to use makedev() in recent GNU C Library versions

--- libarchive/archive_pack_dev.c
+++ libarchive/archive_pack_dev.c
@@ -51,6 +51,9 @@ __RCSID("$NetBSD$");
 #ifdef HAVE_STRING_H
 #include <string.h>
 #endif
+#ifdef HAVE_SYS_SYSMACROS_H
+#include <sys/sysmacros.h>
+#endif
 #ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>
 #endif
